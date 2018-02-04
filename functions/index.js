9f7feca1fcc39d704dea9ffe468b4d98ac922480
中村弘武 <abctail30@gmail.com>
2018-02-04 18:41:53 +0900

    feat: download file by firebase store

--- functions/index.js
+++ functions/index.js
@@ -1,56 +1,23 @@
-// Test Functions
+//使わなくなった笑。一旦キープ。
+'use strict'
 
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+const express = require('express')
+const bodyParser = require('body-parser')
 const request = require('request')
-const functions = require('firebase-functions')
 
-// The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require('firebase-admin')
-admin.initializeApp(functions.config().firebase)
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
-exports.notification = functions.database.ref('room/{name}/stop').onWrite(event => {
-  const val = event.data.val()
-  console.log(val)
-  return request.post(
-    "",
-    {json: {text: `${event.params.name} change !!`}}
-  )
+const server = app.listen(3000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env)
 })
 
-// let payload = {
-//   form: {
-//       token: '',
-//       channel: 'qs-notification',
-//       username: 'abctail30',
-//       attachments: JSON.stringify([
-//         {
-//           "color": "#36a64f",
-//           "pretext": "Q'sで聞き手から反応がありました",
-//           "author_name": "Q's",
-//           "author_link": "http://qiita.com/daikiojm",
-//           "title": "Q's notification",
-//           "text": "待ってが押されました"
-//         }
-//       ])
-//   }
-// }
-//
-// exports.addMessage = functions.https.onRequest((req, res) => {
-//   // Grab the text parameter.
-//   const original = req.query.text
-//
-//   request.post(slack_endpoint, payload, (error, response, body) => {
-//     if (error) {
-//       console.log(error);
-//     } else {
-//       console.log(body);
-//     }
-//   })
-//   // Push the new message into the Realtime Database using the Firebase Admin SDK.
-//   admin.database().ref('/room').push({original: original}).then(snapshot => {
-//     // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-//     res.redirect(303, snapshot.ref)
-//     return false
-//   }).catch(e => {
-//     return e
-//   })
-// })
+exports.generateThumbnail = functions.storage.object().onChange(event => {
+  console.log('waiiii')
+  return
+});
